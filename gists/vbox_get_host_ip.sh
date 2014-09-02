fa7211b659e692426e0168ecf99138ffea88848b
Joe Johnston <joe@simple10.com>
2014-09-02 13:28:12 -0700

    Update ip gists

--- gists/vbox_get_host_ip.sh
+++ gists/vbox_get_host_ip.sh
@@ -3,16 +3,20 @@ set -x
 
 # Get boot2docker host adapter IP from VirtualBox.
 # The host adapter IP is the bridge network IP.
-hostip_get() {
+host_adapter_ip() {
   VM_UUID=$(VBoxManage list runningvms | grep boot2docker | cut -d ' ' -f 2 | sed 's/[{}]//g')
-  # echo "VM_UUID=$VM_UUID"
   VM_HOSTADAPTER=$(VBoxManage showvminfo $VM_UUID --machinereadable | grep hostonlyadapter | cut -d '=' -f 2 | sed 's/\"//g')
-  # echo "VM_HOSTADAPTER=$VM_HOSTADAPTER"
   VM_HOSTADAPTER_IP=$(VBoxManage list hostonlyifs | sed -n -e '/vboxnet0/,/VBoxNetworkName:/ p' | grep IPAddress | tr -d ' ' | cut -d ':' -f 2)
-  # echo "VM_HOSTADAPTER_IP=$VM_HOSTADAPTER_IP"
   echo $VM_HOSTADAPTER_IP
-  #VM_BOOT2DOCKER_IP=$(boot2docker ssh "ifconfig eth1" | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-  #echo $VM_BOOT2DOCKER_IP
 }
 
-echo hostip_get()
+# Get boot2docker IP.
+# This is the IP that docker API runs on. Same as `boot2docker ip`.
+boot2docker_ip() {
+  VM_BOOT2DOCKER_IP=$(boot2docker ssh "ifconfig eth1" | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+  echo $VM_BOOT2DOCKER_IP
+}
+
+
+echo "Host IP: $(host_adapter_ip)"
+echo "Boot2Docker IP: $(boot2docker_ip)"

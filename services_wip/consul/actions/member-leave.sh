2b7e39baf353b546f1f7612b8711152ed564adbf
John Albietz <inthecloud247@gmail.com>
2014-09-04 15:54:37 -0700

    Added services_wip from research branch

---
+++ services_wip/consul/actions/member-leave.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "x${SERF_SELF_ROLE}" != "xlb" ]; then
+    echo "Not an lb. Ignoring member leave"
+    exit 0
+fi
+
+while read line; do
+    NAME=$(echo $line | awk '{print $1 }')
+    sed -i'' "/${NAME} /d" /etc/haproxy/haproxy.cfg
+done
+
+exec /usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)

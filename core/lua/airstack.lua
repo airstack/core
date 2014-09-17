fbe386aa7c8fb2fd563e5309cb1145c36d84f336
Joe Johnston <joe@simple10.com>
2014-09-16 17:32:15 -0700

    Move lua popen3 to airstack.lua

--- core/lua/airstack.lua
+++ core/lua/airstack.lua
@@ -12,7 +12,6 @@
 -- Add core/lua directory to require path
 package.path = package.path .. ";/package/airstack/core/lua/?.lua"
 
-local popen = require("popen")
 local posix = require("posix")
 
 -- Module returned at end of script
@@ -34,10 +33,38 @@ local as = {}
 -- @return multi  stdout, stderr, exit_code, pid
 -- @throws        When command fails
 function as.exec(...)
-  local pid, stdin_fd, stdout_fd, stderr_fd, code = popen.popen3(...)
+  local pid, stdin_fd, stdout_fd, stderr_fd, code = as.popen3(...)
   local stdout = posix.read(stdout_fd, 10000)
   local stderr = posix.read(stderr_fd, 10000)
   return stdout, stderr, code, pid
+end
+
+-- popen3
+-- Modified from https://github.com/kylemanna/lua-popen3
+function as.popen3(path, ...)
+  local r1, w1 = posix.pipe()
+  local r2, w2 = posix.pipe()
+  local r3, w3 = posix.pipe()
+  assert((w1 ~= nil and r2 ~= nil and r3 ~= nil), "pipe() failed")
+  local pid, err = posix.fork()
+  assert(pid ~= nil, "fork() failed")
+  if pid == 0 then
+    posix.close(w1)
+    posix.close(r2)
+    posix.close(r3)
+    posix.dup2(r1, posix.fileno(io.stdin))
+    posix.dup2(w2, posix.fileno(io.stdout))
+    posix.dup2(w3, posix.fileno(io.stderr))
+    local ret, err = posix.execp(path, ...)
+    assert(ret ~= nil, "execp() failed")
+    posix._exit(ret)
+    return
+  end
+  local cpid, status, code = posix.wait()
+  posix.close(r1)
+  posix.close(w2)
+  posix.close(w3)
+  return pid, w1, r2, r3, code
 end
 
 
